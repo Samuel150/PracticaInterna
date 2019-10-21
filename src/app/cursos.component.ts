7def79565e6e7fe2b8bd75e6c7cd75e5a9badb43
Samuel <hsamueljackson4@gmail.com>
2019-10-21 15:34:35 -0400

    first Commit

--- src/app/cursos.component.ts
+++ src/app/cursos.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core'
 @Component({
   selector: 'cursos',
   template: `
-  <h1>Mis cursos</h1>
+  <h1>Mis commits</h1>
   <p>TypeScript</p>
   `,
   styles: ['h1 { color: red; }']
