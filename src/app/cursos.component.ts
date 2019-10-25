42cf8be8706d43647c63c770579edb492b636a9c
Samuel <hsamueljackson4@gmail.com>
2019-10-24 21:57:40 -0400

    ultimate intellij

--- src/app/cursos.component.ts
+++ src/app/cursos.component.ts
@@ -3,8 +3,9 @@ import {Component} from '@angular/core'
 @Component({
   selector: 'cursos',
   template: `
-  <h1>Mi Commit</h1>
-  <p>TypeScript</p>
+  <h1>Mi commit</h1>
+  <p>TypeScript !!</p>
+
   `,
   styles: ['h1 { color: red; }']
 })
