36ec7858993a4035fa69e2d2308d8a806b3b8060
Samuel <hsamueljackson4@gmail.com>
2019-10-30 22:37:16 -0400

    ultimate intellij

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInButtonComponent } from './log-in-button/log-in-button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from "./material/material.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { LogInButtonComponent } from './log-in-button/log-in-button.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
